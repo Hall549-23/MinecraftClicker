cf4be22057142063813e5a53cde995433b49bc50
unknown <maks.slyzar@gmail.com>
2020-05-23 23:17:01 +0300

    l

--- public/game.js
+++ public/game.js
@@ -5,13 +5,17 @@ function Game(){
         y: -300
     }
     now_block = {x:0,y:0}
+    show_inventory = true;
+    inventory = [
+        
+    ]
 
     function draw() {
         let width_block = 48;
         let height_block = 48;
         for (var i = 0; i < this.blocks.length; i++){
             for (var k = 0; k < this.blocks[i].length; k++){
-                ctx.drawImage(textures_img, this.blocks[i][k].x*width_block,
+                ctx.drawImage(textures_blocks, this.blocks[i][k].x*width_block,
                                             this.blocks[i][k].y*height_block,
                                             width_block,
                                             height_block,
@@ -20,7 +24,7 @@ function Game(){
                                             width_block,
                                             height_block);
                 
-                ctx.drawImage(textures_img, Math.floor(blocks[i][k].proc / (blocks[i][k].procFull  / 9))*width_block,
+                ctx.drawImage(textures_blocks, Math.floor(blocks[i][k].proc / (blocks[i][k].procFull  / 8))*width_block,
                                             15*height_block,
                                             width_block,
                                             height_block,
@@ -42,52 +46,103 @@ function Game(){
 
     function SpawnArray() {
         for (var i = 0; i < 20; i++){
-            obj = {
-                x: blocks_ids.grass.x,
-                y: blocks_ids.grass.y,
-                proc: 0,
-                procFull: blocks_ids.grass.procFull
-            }
-            blocks[blocks.length - 1].push(obj);
+            blocks[blocks.length - 1].push(AllObjects.getObjectByName('grass'));
         }
         for (var i = 0; i < 7; i++){
             blocks.push([])
             for (var k = 0; k < 20; k++){
-                blocks[blocks.length-1].push(blocks_ids.dirt);
+                blocks[blocks.length-1].push(AllObjects.getObjectByName('dirt'));
             }
         }
         for (var i = 0; i < 50; i++){
             blocks.push([])
             for (var k = 0; k < 20; k++){
-                blocks[blocks.length-1].push(blocks_ids.stone);
+                blocks[blocks.length-1].push(AllObjects.getObjectByName('stone'));
             }
         }
+        console.log(blocks)
     }
 
     function settings() {
-        button.style = 'top: ' + (canvas.height - 120) + 'px;'+
+        button.style = 'top: ' + (canvas.height - 150) + 'px;'+
                        'left: ' + (canvas.width/2 - 50) + 'px;';
-        chest.style = 'left: ' + (canvas.width - 120) + 'px';
-        }
+        chest.style = 'left: ' + (canvas.width - 100) + 'px';
+        $('#gameDiv #shop').css({
+            'top':120+'px',
+            'left':(canvas.width-100)+'px'
+        });
+        $('#gameDiv #inventory').css({
+            'width':canvas.width-(canvas.width/100*4),
+            'height':canvas.width/2,
+            'left':canvas.width/100 * 2,
+            'top':canvas.height-canvas.width/2-50,
+        });
+        $('#inventory .slots .row').css({
+            
+        });
+    }
 
     button.onclick = function(e) {
         if (blocks[now_block.y][now_block.x].proc <= blocks[now_block.y][now_block.x].procFull)
-            blocks[now_block.y][now_block.x].proc += 5;
+            blocks[now_block.y][now_block.x].proc += 20;
         else{
-            blocks[now_block.y][now_block.x] = blocks_ids.empty;
+            blocks[now_block.y][now_block.x] = AllObjects.getObjectByName('empty');
             now_block.x+=1;
         }
         //console.log(blocks[now_block.y][now_block.x+2])
     }
+    $('#gameDiv #chest').click(function(){
+        if (show_inventory){
+            $('#gameDiv #inventory').fadeIn(500);
+            show_inventory = false;
+        }
+        else{
+            $('#gameDiv #inventory').fadeOut(500);
+            show_inventory = true;
+        }
+        console.log('Test')
+    });
+    $('#gameDiv #canvas').click(function(){
+        $('#gameDiv #inventory').fadeOut(500);
+    });
 
+    function createInventrory(){
+        for (var i = 0; i < 3; i++){
+            inventory.push([]);
+            for (var k = 0; k < 9; k++){
+                inventory[i].push({
+                    item:AllObjects.getObjectByName('grass'),
+                    num:0,
+                });
+            }
+        }
+    }
+    function drawInventory(){
+        for (var i = 0; i < inventory.length; i++){
+            $('#inventory .slots').append('<div class="row" id='+'row_'+i+'></div>');
+            for (var k = 0; k < inventory[i].length; k++){
+                if (inventory[i][k].item != null)
+                    $('#inventory .slots #row_'+i).append('<div><img x=0 width=100% src="'+inventory[i][k].item.img.src+'"></div>');
+                else
+                    $('#inventory .slots #row_'+i).append('<div></div>');
+                    
+                $('#inventory .slots .row div').css({
+                    'width':'',
+                    'height':''
+                })
+            }
+        }
+        
+    }
 
-
+    createInventrory();
+    drawInventory();
     SpawnArray();
     update();
 }
 
-var textures_img = new Image();
-textures_img.src = 'img/textures.png';
-textures_img.onload = function() {
+
+
+textures_blocks.onload = function() {
     Game();       
 }
--- public/settings.js
+++ public/settings.js
@@ -1,5 +1,11 @@
 var ctx = canvas.getContext('2d');
 
+//LOAD TEXTURES
+var textures_blocks = new Image();
+var textures_tools = new Image();
+textures_blocks.src = 'img/textures.png';
+textures_tools.src = 'img/textures_tools.png';
+
 if (window.innerWidth < 700){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -12,29 +18,76 @@ window.onload = function() {
 	Gifffer();
 }
 
-var blocks_ids = {
-    empty: {
-        x:0,
-        y:15,
-        proc:0,
-        procFull:0
-    },
-    grass: {
-        x: 3,
-        y: 0,
-        proc: 0,
-        procFull: 100
-    },
-    dirt: {
-        x: 2,
-        y: 0,
-        proc: 0,
-        procFull: 50
-    },
-    stone: {
-        x: 1,
-        y: 0,
-        proc: 0,
-        procFull: 500
+
+
+class all_objects {
+    objects = [
+        {
+            img:textures_blocks,
+            x:0,
+            y:15,
+            proc:0,
+            procFull:0,
+            name:'Empty',
+            random_key:null,
+            full: 64
+        },
+        {
+            img:textures_blocks,
+            x: 3,
+            y: 0,
+            proc: 0,
+            procFull: 100,
+            name:'Grass',
+            random_key:null,
+            full: 64
+        },
+        {
+            img:textures_blocks,
+            x: 2,
+            y: 0,
+            proc: 0,
+            procFull: 50,
+            name:'Dirt',
+            random_key:null,
+            full: 64
+        },
+        {
+            img:textures_blocks,
+            x: 1,
+            y: 0,
+            proc: 0,
+            procFull: 500,
+            name: 'Stone',
+            random_key:null,
+            full: 64
+        }
+    ];
+
+    getObjectByName (name){
+        let output = {}
+        this.objects.forEach(element => {
+            if (name.toLowerCase() == element.name.toLowerCase()){
+                console.log(element)
+                output = clone(element);
+            }
+        });
+        
+        let key = this.generateRandomKey();
+        output.random_key = key;
+        return output
+    }
+    generateRandomKey (){
+        return Math.floor(Math.random()*10000000000000)
     }
 }
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+var AllObjects = new all_objects();
